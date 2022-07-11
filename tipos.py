c27d60ada49b8dc63578169524b4b81fbf895a9f
felipeflorianof <felipeflorianof@gmail.com>
2022-07-11 20:15:02 -0300

    Atividades do curso Softex-Pernambuco

---
+++ tipos.py
@@ -0,0 +1,25 @@
+n1 = 5
+n2 = 5.7
+txt = "String"
+booleano = False
+
+print("Este é um tipo ")
+print(type(n1))
+print("Este é um tipo ")
+print(type(n1))
+print("Este é um tipo ")
+print(type(txt))
+print("Este é um tipo ")
+print(type(booleano))
+
+var = 100
+
+print("Esta é a primeira aparição da variável")
+print(var)
+print("Esta é a segunda aparição da variável")
+print(var)
+print("Esta é a terceira aparição da variável")
+print(var)
+print("Esta é a quarta aparição da variável")
+print(var)
+print("E você só definiu o valor uma única vez")
