5acc86183816328f38249a8c99a1e88a890ee935
Felipe Floriano de Fontes <102492775+felipeflorianof@users.noreply.github.com>
2022-07-20 18:39:47 -0300

    Atividades do curso Softex-Pernambuco

---
+++ 4-modulo/ex01/Estudar.php
@@ -0,0 +1,26 @@
+<?php
+
+    class Estudar{
+        public $estudando;
+        public $assunto;
+        public $descansar;
+
+
+        function estudando(){
+            if($this->estudando){
+                $this->estudando = false;
+            }else{
+                $this->estudando = true;
+            }
+        }
+
+        function assunto($assunto){
+            $this->assunto = $assunto;
+            echo "<p>atualmente estudando ", $assunto, "</p>";
+        }
+
+        function descansar(){
+            $this->estudando = false;
+            echo "<p>Indo dormir...</p>";
+        }
+    }
