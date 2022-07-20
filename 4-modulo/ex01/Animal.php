db853e0675a6dcff203217f58ed3a3a57a34cd11
Felipe Floriano de Fontes <102492775+felipeflorianof@users.noreply.github.com>
2022-07-20 18:42:50 -0300

    Atividades do curso Softex-Pernambuco

---
+++ 4-modulo/ex01/Animal.php
@@ -0,0 +1,19 @@
+<?php
+
+    class Animal{
+        public $peso;
+        public $idade;
+        public $especie;
+
+        function peso($peso){
+            $this->peso = $peso;
+        }
+
+        function idade($idade){
+            $this->idade = $idade;
+        }
+
+        function especie($especie){
+            $this->especie = $especie;
+        }
+    }
