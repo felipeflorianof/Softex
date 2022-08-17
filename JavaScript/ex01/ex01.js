da8157c26a7dc41df7edc809755d588b8a238821
felipeflorianof <felipeflorianof@gmail.com>
2022-08-17 20:36:36 -0300

    Atividades do curso Softex-Pernambuco

---
+++ JavaScript/ex01/ex01.js
@@ -0,0 +1,32 @@
+/* Exemplo - 1
+
+botão de pesquisa do Google ->
+input que vai receber os dados que o usuário quer pesquisar: 
+
+<input id="input" type="search" autocomplete="off" spellcheck="false" role="combobox" placeholder="Pesquise no Google ou escreva um URL" aria-live="polite">
+*/ 
+
+/* querySelector que vai pegar o que foi digitado no input, e apartir de outra série de códigos vai executar algo dependendo do que o usuário digitou:
+
+
+document.querySelector("body > ntp-app").shadowRoot.querySelector("#realbox").shadowRoot.querySelector("#input")
+*/
+
+
+
+
+/** Exemplo - 2
+ * 
+ *  Botão de uma vaga do linkedIn:
+ * <button id="ember291" class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view" type="button"><!---->
+<span class="artdeco-button__text">
+    
+              <!---->Candidate-se logo<!---->
+            
+</span></button>
+ * 
+ * querySelector que vai executar uma função quando eu clicar no botão:
+ * document.querySelector("#ember291")
+ */
+
+
